66470cc6cd61b8cdcb86d8c38f6f95c9f2d0b0f7
Brandon <brandon121j@gmail.com>
2021-11-03 12:50:33 -0400

    Completion of LastNameHooks.js file and importation of file in Singup.js

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import FirstNameHooks from '../../hooks/FirstNameHooks'
+import LastNameHooks from '../../hooks/LastNameHooks';
 
 function Signup() {
 
-    const [firstName, handleFirstNameOnChange, firstNameError] = FirstNameHooks()
+    const [firstName, handleFirstNameOnChange, firstNameError] = FirstNameHooks();
+    const [LastName, handleLastNAmeOnChange, LastNameError, setOnFocus] = LastNameHooks();
 
     return (
         <div>
             <form>
                 <input name='firstName' />
                 <div>{firstNameError && firstNameError}</div>
+                <input name='lastName' onChange={handleLastNAmeOnChange} onFocus={() => setOnFocus(true)}/>
+                <div>{lastNameError && lastNameError}</div>
             </form>
         </div>
     )
--- src/hooks/LastNameHooks.js
+++ src/hooks/LastNameHooks.js
@@ -1,12 +1,26 @@
 import { useState, useEffect } from 'react';
+import { isAlpha } from 'validator';
 
 function LastNameHooks() {
-    const [lastName, setLastName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [error, setError] = useState('');
+	const [onFocus, setOnFocus] = useState(false);
 
-    useEffect(() => {
+	useEffect(() => {
+		if (onFocus) {
+			if (lastName.length > 0) {
+				if (!isAlpha(lastName)) {
+					setError('Cannot have special character or number');
+				}
+			}
+		}
+	}, [lastName, onFocus]);
 
-    }, [lastName]);
-    
+	function handleLastNAmeOnChange(e) {
+		setLastName(e.target.value);
+	}
 
-    
+	return [lastName, handleLastNAmeOnChange, error, setOnFocus];
 }
+
+export default LastNameHooks;
