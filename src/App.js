5d2cebdba25337c91e00452547514b044bb4b1f1
Brandon <brandon121j@gmail.com>
2021-11-10 20:56:14 -0500

    Changes made to App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import jwtDecode from 'jwt-decode';
 import './App.css';
+
 import Signin from './components/signin/Signin';
 import Signup from './components/signup/Signup';
 import Nav from './components/nav/Nav';
 
 function App() {
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		let jwtToken = window.localStorage.getItem('jwtToken');
+
+		if (jwtToken) {
+			let decodedToken = jwtDecode(jwtToken);
+
+			const currentTime = Date.now() / 1000;
+
+			if (decodedToken.exp < currentTime) {
+				window.localStorage.removeItem('jwtToken');
+				setUser(null)
+			} else {
+				setUser({
+					email: decodedToken.email,
+					password: decodedToken.password
+				})
+			}
+		}
+	})
+
+
+
+
+
 	return (
 		<div className="App">
 			<ToastContainer theme='colored' />
